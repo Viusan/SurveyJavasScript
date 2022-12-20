d82f0f979389f5dd0637fc89d7c280d50e035f5b
ViusanSantos <viusansantos@gmail.com>
2022-12-20 11:06:40 +0100

    comments

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
+//Varibler for radio knappene
 var himmel = document.querySelector("#himmelNei");
-
 var flat = document.querySelector("#flatNei");
-
 var venus = document.querySelector("#venusNei");
-
 var saturn = document.querySelector("#saturnNei");
 
+//Variabler for hvis folk er enig med spørsmålene
 var himmelSvar = 0;
 var jordenSvar = 0;
 var venusSvar = 0;
 var saturnSvar = 0;
 var antallBrukereSomHarSvart = 0;
 
-function svar(){
+function svar(){//Hvis de er enige, økes variablene som brukes som statistikk
   if(himmel.value == 1){
     console.log("du er enig med at himmelen er grønn")
     himmelSvar++;
@@ -40,15 +39,17 @@ function svar(){
   }else{
     console.log("saturn er flat")
   }
+  //Teller hvor mange som svarer på undersøkelsen
   antallBrukereSomHarSvart++
 }
 
-function visResultat(){
+function visResultat(){//Lager chart når du trykker på vis resultat knappen
+  //Variabler som blir brukt til charten
   var xValues = ["Himmelen er grønn", "Jorden er flat", "Venus er firkantet", "Saturn er rund"];
   var yValues = [(himmelSvar/antallBrukereSomHarSvart)*100, (jordenSvar/antallBrukereSomHarSvart)*100, (venusSvar/antallBrukereSomHarSvart)*100, (saturnSvar/antallBrukereSomHarSvart)*100];
   var barColors = ["red", "green","blue","orange","brown"];
   
-  new Chart("myChart", {
+  new Chart("myChart", { //Koden som lager chart
     type: "bar",
     data: {
       labels: xValues,
@@ -73,5 +74,6 @@ function visResultat(){
       }
     }
   });
+  //Tekst som viser hvor mange som har tatt undersøkelsen
   document.getElementById("antallBrukereDisplay").innerHTML = "Antall bruker som har tatt spørreundersøkelsen: " + antallBrukereSomHarSvart;
 }
